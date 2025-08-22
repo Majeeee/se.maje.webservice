182caa58f0ade10048122a9830d53101742507f6
Majeeee <majid.tajik@stud.sti.se>
2025-08-22 13:33:48 +0200

    fix package.jason

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-
 import express from "express"
+import "dotenv/config"
 
 const app = express()
- const port: number = 3000
- app.get("/", (request, response) => {
- response.send("Hello world!")
- })
- app.listen(port, () => {
- console.log(`Listening to port ${port}`)
- })
+const port: number = Number(process.env.PORT) || 3000 // Could cras
+const secret = process.env.MY_GLOBAL_TEST_SECRET // Check later as intellisense doesn't work
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world!")
+})
+
+// Start Server on Port Variable
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+  console.log(secret)
+})
