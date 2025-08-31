8f16e2b3cb8911ec44debf42beb2523070dee68d
Majeeee <Majid.Tajik@stud.sti.se>
2025-08-31 21:23:23 +0200

    Databas och render är klara

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,36 @@
-
 import express from "express"
+import "dotenv/config"
+import { closeDB, runDB } from "./db/database.js"
 
 const app = express()
- const port: number = 3000
- app.get("/", (request, response) => {
- response.send("Hello world!")
- })
- app.listen(port, () => {
- console.log(`Listening to port ${port}`)
- })
+const port: number = Number(process.env.PORT) || 3000 // Could crash
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world!")
+})
+
+// Start Server on Port Variable
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
+// Server startup async function - goes here
+
+
+async function startServer() {
+try {
+await runDB()
+app.listen(port, () => {
+console.log(`Listening to port ${port}`)
+console.log(`Start the app: http://localhost:${port}`)
+})
+process.on("SIGINT", async () => {
+console.log("Cleaning up...")
+await closeDB()
+process.exit(0)
+})
+} catch (error) {
+console.log(error)
+}
+}
+startServer()
