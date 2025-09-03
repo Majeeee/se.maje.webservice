874757f986f093a973d757ee00fbc13a05eba59e
Majeeee <Majid.Tajik@stud.sti.se>
2025-09-03 23:01:58 +0200

    First Commit

---
+++ Documents/javaprojects/Webservice/WS_Uppgifter_Lektion_3/WebService_Uppgifter_Lektion_3/src/main/java/se/maje/webservice/WebService_Uppgifter_Lektion_3/WebServiceUppgifterLektion3Application.java
@@ -0,0 +1,13 @@
+package se.maje.webservice.WebService_Uppgifter_Lektion_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceUppgifterLektion3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceUppgifterLektion3Application.class, args);
+	}
+
+}
---
+++ Documents/javaprojects/Webservice/WS_Uppgifter_Lektion_3/WebService_Uppgifter_Lektion_3/src/test/java/se/maje/webservice/WebService_Uppgifter_Lektion_3/WebServiceUppgifterLektion3ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.maje.webservice.WebService_Uppgifter_Lektion_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceUppgifterLektion3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
