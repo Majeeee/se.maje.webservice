48ceae3950df554984cb03ae40dd62fb23cc7cf1
Majeeee <Majid.Tajik@stud.sti.se>
2025-09-07 00:56:20 +0200

    Första commit uppgift 1

---
+++ Documents/javaprojects/Webservice/WS_Uppgifter_Lektion_4/WS_Uppgifter_Lektion_4/src/main/java/se/maje/webservice/WS_Uppgifter_Lektion_4/WsUppgifterLektion4Application.java
@@ -0,0 +1,13 @@
+package se.maje.webservice.WS_Uppgifter_Lektion_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsUppgifterLektion4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsUppgifterLektion4Application.class, args);
+	}
+
+}
---
+++ Documents/javaprojects/Webservice/WS_Uppgifter_Lektion_4/WS_Uppgifter_Lektion_4/src/test/java/se/maje/webservice/WS_Uppgifter_Lektion_4/WsUppgifterLektion4ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.maje.webservice.WS_Uppgifter_Lektion_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsUppgifterLektion4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
