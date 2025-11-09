e802b80e6be740854c2658d15e3294fbb3cb217a
Majeeee <Majid.Tajik@stud.sti.se>
2025-11-09 02:03:44 +0100

    uppgift 5 -- Lektion 8  WebService project

---
+++ Documents/javaprojects/Webservice/WebService_Uppgifter_Lektion_8/src/main/java/se/maje/WebService_Uppgifter_Lektion_8/controller/StudentController.java
@@ -0,0 +1,40 @@
+package se.maje.WebService_Uppgifter_Lektion_8.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.maje.WebService_Uppgifter_Lektion_8.model.Student;
+import se.maje.WebService_Uppgifter_Lektion_8.repository.StudentRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable Long id) {
+        Optional<Student> student = studentRepository.findStudentById(id);
+
+        if (student.isPresent()) {
+            return ResponseEntity.ok(student.get());
+        } else {
+            return ResponseEntity.status(404)
+                    .body("Student med ID " + id + " finns inte.");
+        }
+    }
+}
