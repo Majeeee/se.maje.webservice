4520f1979b0ebaaaaafffc5661a9c4b5eeaa67ae
Majeeee <Majid.Tajik@stud.sti.se>
2025-11-09 01:44:58 +0100

    Initial commit – Lektion 8  WebService project

---
+++ Documents/javaprojects/Webservice/WebService_Uppgifter_Lektion_8/src/main/java/se/maje/WebService_Uppgifter_Lektion_8/WebServiceUppgifterLektion8Application.java
@@ -0,0 +1,13 @@
+package se.maje.WebService_Uppgifter_Lektion_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceUppgifterLektion8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceUppgifterLektion8Application.class, args);
+	}
+
+}
